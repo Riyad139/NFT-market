097d43ed6231b9abb3000dabc1cc2ee5d6db8f51
riyad139 <riyad.ahmed1886@gmail.com>
2022-07-20 17:56:56 +0600

    icon added

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 import Button from "../Buttons/Button";
 import ButtonFill from "../Buttons/ButtonFill";
 
 export default function NavBar(props) {
   return (
     <nav className="w-full py-5 px-9 justify-between items-center flex">
-      <strong className="font-headers text-xl tracking-wide">NFT Market</strong>
+      <div className="space-x-2 flex items-center">
+        <Image src="/HeaderIcon/Sygnet.svg" alt="" width={22} height={22} />
+        <strong className="font-headers text-xl tracking-wide">
+          NFT Market
+        </strong>
+      </div>
       <div className="flex text-sm font-medium text-gray-500 space-x-10">
         <Link href="#">Auctions</Link>
         <Link href="#">Roadmap</Link>
